acdb8aec9efe3e3be693ea042199ac6922ca1319
Kateryna-gr <128936446+Kateryna-gr@users.noreply.github.com>
2023-08-20 15:08:56 +0300

    task3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('form');
+const btn = document.querySelector('[type="submit"]');
+
+btn.addEventListener('click', handler);
+
+function handler(evt){
+  evt.preventDefault();
+  let delay = parseInt(form.elements.delay.value);
+  let step = parseInt(form.elements.step.value);
+  let amount = parseInt(form.elements.amount.value);
+  console.log(delay, step, amount);
+
+  for (let i = 1; i <= amount; i += 1, delay += step) {
+    setTimeout(() => createPromise(i, delay), delay);
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
   } else {
-    // Reject
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
   }
 }
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     // Fulfill
+//   } else {
+//     // Reject
+//   }
+// }
+
+// createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
+
